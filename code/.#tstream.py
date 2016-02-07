e2d7529601db5264b59f078e8400fec4785179f4
xuewunvs <xuewunvs@gmail.com>
2016-02-06 19:54:51 -0500

    got Streaming Tweets -unformated

---
+++ code/.#tstream.py
@@ -0,0 +1,1 @@
+XueWang@dyn-129-236-211-97.dyn.columbia.edu.8984
---
+++ code/tt3.py
@@ -0,0 +1,14 @@
+from TwitterAPI import TwitterAPI
+
+CONSUMER_KEY = '8aIDWdGQuFgC9oypr2yozz6KY'
+CONSUMER_SECRET = 'tn3FJdxt22QrHgim07mmaWOxFhNCtj7BB3pAjNVEw9aVZyECWC'
+ACCESS_TOKEN_KEY = '4883184502-tPuS4S7rC0gEqpGq08EFtkUo1HYTZbskez2YtYu'
+ACCESS_TOKEN_SECRET = '0xS7drlYHYb32ASOCEeeVo8Qx405RAreGkv0S2ARNsVLz'
+
+
+api = TwitterAPI(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN_KEY,ACCESS_TOKEN_SECRET)
+
+
+r = api.request('statuses/filter', {'locations':'-74,40,-73,41'})
+for item in r:
+        print(item)
